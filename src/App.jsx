0948c9e78ebf57c89e3e2e3de5e08c9828b7a6af
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-07-16 21:31:56 -0400

    Added mock appointments data and display them

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
-import { BiCalendar } from "react-icons/bi"
+import { BiCalendar, BiTrash } from "react-icons/bi"
 import Search from "./components/Search"
 import AddAppointment from "./components/AddAppointment"
-
+import appointmentList from "./data/appointments.json"
 
 function App() {
   return (
@@ -11,6 +11,25 @@ function App() {
         <BiCalendar className="inline-block text-red-400 align-middle" />Your Appointments</h1>
       <AddAppointment />
       <Search />
+
+      <ul className="divide-y divide-gray-200">
+        {appointmentList.map(appointment => (
+          <li key={appointment.id} className="px-3 py-3 flex items-start">
+            <button type='button'
+              className="p-1.5 mr-1.5 mt-1 rounded text-whhite bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                <BiTrash className='inline-block align-baseline'/></button>
+                <div className="flex-grow">
+                  <div className='flex items-start'>
+                    <span className='span-none font-medium text-2xl text-blue-500'>{appointment.petName}</span>
+                    <span className="flex-grow text-right">{appointment.aptDate}</span>
+                  </div>
+                  <div><b className='font-bold text-blue-500'>Owner:</b> {appointment.ownerName}</div>
+                  <div className='leading-tight'>{appointment.aptNotes}</div>
+                </div>
+          </li>
+        ))}
+      </ul>
+
     </div>
   )
 }
