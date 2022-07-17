d399530bf956ba4e7f0ea8255708a148af02e0b5
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-07-16 23:25:25 -0400

    Fetching mock appointment data like it is coming from a server

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,36 @@ import './App.css'
 import { BiCalendar } from "react-icons/bi"
 import Search from "./components/Search"
 import AddAppointment from "./components/AddAppointment"
-import appointmentList from "./data/appointments.json"
+// import appointmentList from "./data/appointments.json"
 
 import AppointmentInfo from './components/AppointmentInfo'
+import { useEffect, useState } from 'react'
+import { useCallback } from 'react'
 
 function App() {
+  let global = 0;
+
+  const [appointmentList, setAppointmentList] = useState([]);
+
+  const fetcthAppointments = useCallback(
+    () => {
+      console.log(`useCallback: fetching appointments ${global++}`);
+      fetch("./data/appointments.json")
+        .then(res => res.json())
+        .then(data =>  { setAppointmentList(data) })
+    }, []
+  )
+
+  useEffect(() => {
+    console.log(`useEffect: fetching appointments ${global++}`);
+    fetcthAppointments()
+  }, [fetcthAppointments])
+
+  const onDeleteAppointment = (appointmentId) => {
+    const newAppointmentList = appointmentList.filter(appointment => appointment.id !== appointmentId)
+    setAppointmentList(newAppointmentList)
+  }
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="font-medium leading-tight text-5xl mt-0 mb-2">
@@ -16,7 +41,7 @@ function App() {
 
       <ul className="divide-y divide-gray-200">
         {appointmentList.map(appointment => (
-          <AppointmentInfo key={appointment.id} appointment={appointment} />
+          <AppointmentInfo onDeleteAppointment={onDeleteAppointment} key={appointment.id} appointment={appointment} />
         ))}
       </ul>
 
